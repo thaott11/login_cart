2cde3d9c0cd037ded1b1c0089e95c836bb631ba5
thaott11 <luuvanthao524@gmail.com>
2024-07-25 10:30:39 +0700

    name

--- DemoGH_OnTap/Controllers/AccountController.cs
+++ DemoGH_OnTap/Controllers/AccountController.cs
@@ -10,11 +10,7 @@ namespace DemoGH_OnTap.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-
+       
         public IActionResult DangKy() // tạo ra view đăng kí thui
         {
             return View();
@@ -63,9 +59,94 @@ namespace DemoGH_OnTap.Controllers
             else // trong trường hợp thành công sẽ trả về trang chủ
             {
                 HttpContext.Session.SetString("username", userName); // Lưu dữ liệu login vào Session
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Account");
             }
 
+        }
+
+        // hiển thị tất cả danh sánh account
+        public IActionResult Index(string name)
+        {
+            // lấy giá trị có tên Account
+            var session = HttpContext.Session.GetString("username");
+            if(session == null)
+            {
+                ViewData["message"] = "bạn chưa đăng nhập hoặc đăng nhập hết hạn";
+            }
+            else
+            {
+                ViewData["message"] = $"Xin chào {session}";
+            }
+            // lấy toàn bộ account 
+            var accountdata = _db.Accounts.ToList();
+            // làm phần tìm kiếm 
+            // nếu mà name tìm kiếm rỗng thì sẽ trả về toàn bộ dữ liệu
+            if(string.IsNullOrEmpty(name))
+            {
+                return View(accountdata);
+            }
+            else
+            {
+                var seachdata = _db.Accounts.Where(x => x.Name.Contains(name)).ToList();
+                // lưu số lượng kết quả tìm kiếm trong viewdata và view bang
+                ViewData["count"]= seachdata.Count;
+                ViewBag.Count = seachdata.Count;
+                if(seachdata.Count == 0)
+                {
+                    return View(accountdata) ;
+                }
+                else
+                {
+                    return View(seachdata);
+                }
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Account acc)
+        {
+            _db.Add(acc);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Deltai(Guid id)
+        {
+            var acc = _db.Accounts.FirstOrDefault(x => x.Id == id);
+            return View(acc);
+        }
+
+        public IActionResult Update(Guid id)
+        {
+            var acc = _db.Accounts.FirstOrDefault(x => x.Id == id);
+            return View(acc);
+        }
+        [HttpPost]
+        public IActionResult Update(Account acc, Guid id)
+        {
+            var update = _db.Accounts.FirstOrDefault(x => x.Id == id);
+            update.Name = acc.Name;
+            update.UserName = acc.UserName;
+            update.Password = acc.Password;
+            update.Email = acc.Email;
+            update.Phone  = acc.Phone;
+            update.Address = acc.Address;
+            _db.Accounts.Update(update);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }   
+
+        
+        public IActionResult remove(Guid id)
+        {
+            var remove = _db.Accounts.FirstOrDefault(y => y.Id == id);
+            _db.Accounts.Remove(remove);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
